02cd9a1971c2fb20b9dbaaa9dbe0920bd3c7cb33
Johandarkness <70907376+Johandarkness@users.noreply.github.com>
2024-05-09 00:21:50 -0500

    Mejora botones aplicación
    Se mejoran la estética de los botones de la app.

--- Cliente/app/(panel)/crear-pedido/page.tsx
+++ Cliente/app/(panel)/crear-pedido/page.tsx
@@ -148,15 +148,15 @@ export default function CrearPedido() {
                         </div>
 
                         <div className="w-full flex justify-center items-center mt-5">
-                            <Link href={"/dashboard"} className="px-4 py-2 bg-blue-500 text-white rounded-md">
+                            <Link href={"/dashboard"} className="px-4  btn-sm text-white bg-blue-500 hover:bg-blue-700 rounded-md ml-6 mb-3">
                                 Regresar a Pedidos
                             </Link>
 
-                            <button className="px-4 py-2 bg-red-500 text-white rounded-md ml-2" onClick={handleLimpiarCampos}>
+                            <button className="px-4 py-2 btn-sm text-white bg-red-500 hover:bg-red-700 rounded-md ml-6 mb-3" onClick={handleLimpiarCampos}>
                                 Limpiar campos
                             </button>
 
-                            <button className="px-4 py-2 bg-green-500 text-white rounded-md ml-2" onClick={handleGuardarPedido}>
+                            <button className="px-4 py-2 btn-sm text-white bg-green-500 hover:bg-green-700 rounded-md ml-6 mb-3" onClick={handleGuardarPedido}>
                                 Guardar Pedido
                             </button>
                         </div>
