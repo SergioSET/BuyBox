4e226e8411ebc7a3412c0f9f2133f0e7bf2e4dd2
Steban01 <esteban.cortes@correounivalle.edu.co>
2024-05-20 16:08:50 -0500

    update register photo

--- Cliente/app/(auth)/signup/page.tsx
+++ Cliente/app/(auth)/signup/page.tsx
@@ -11,7 +11,6 @@ export default function SignUp() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState<string | null>(null);
 
-
   const handleSubmit = async (event: { preventDefault: () => void; }) => {
     event.preventDefault();
 
@@ -43,65 +42,69 @@ export default function SignUp() {
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h1 className="h1">Bienvenido al registro.</h1>
           </div>
-          <div className="max-w-sm mx-auto">
-            <form onSubmit={handleSubmit}>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="username">
-                    Usuario
-                  </label>
-                  <input
-                    id="name"
-                    type="text"
-                    className="form-input w-full text-gray-300"
-                    placeholder="Usuario"
-                    required
-                    value={name}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="password">
-                    Contraseña
-                  </label>
-                  <input
-                    id="password"
-                    type="password"
-                    className="form-input w-full text-gray-300"
-                    placeholder="Contraseña (Al menos 10 caracteres)"
-                    required
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <div className="flex justify-between">
-                    <label className="flex items-center">
-                      <input type="checkbox" className="form-checkbox" />
-                      <span className="text-gray-400 ml-2">Recuérdame</span>
+          <div className="flex max-w-4xl mx-auto">
+            <div className="w-1/2 hidden md:block">
+              <img src="/path-to-your-image.jpg" alt="Imagen de registro" className="object-cover h-full w-full" />
+            </div>
+            <div className="w-full md:w-1/2">
+              <form onSubmit={handleSubmit}>
+                <div className="flex flex-wrap -mx-3 mb-4">
+                  <div className="w-full px-3">
+                    <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="username">
+                      Usuario
                     </label>
-                    zz
+                    <input
+                      id="name"
+                      type="text"
+                      className="form-input w-full text-gray-300"
+                      placeholder="Usuario"
+                      required
+                      value={name}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
                   </div>
                 </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mt-6">
-                <div className="w-full px-3">
-                  <button type="submit" className="btn text-white bg-purple-600 hover:bg-purple-700 w-full">
-                    Registrate
-                  </button>
+                <div className="flex flex-wrap -mx-3 mb-4">
+                  <div className="w-full px-3">
+                    <label className="block text-gray-300 text-sm font-medium mb-1" htmlFor="password">
+                      Contraseña
+                    </label>
+                    <input
+                      id="password"
+                      type="password"
+                      className="form-input w-full text-gray-300"
+                      placeholder="Contraseña (Al menos 10 caracteres)"
+                      required
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
                 </div>
+                <div className="flex flex-wrap -mx-3 mb-4">
+                  <div className="w-full px-3">
+                    <div className="flex justify-between">
+                      <label className="flex items-center">
+                        <input type="checkbox" className="form-checkbox" />
+                        <span className="text-gray-400 ml-2">Recuérdame</span>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mt-6">
+                  <div className="w-full px-3">
+                    <button type="submit" className="btn text-white bg-purple-600 hover:bg-purple-700 w-full">
+                      Registrate
+                    </button>
+                  </div>
+                </div>
+                {error && <p className="text-red-500 text-center mt-4">{error}</p>}
+              </form>
+              <div className="text-gray-400 text-center mt-6">
+                Ya tienes una cuenta?{' '}
+                <Link href="/signin" className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out">
+                  Inicia sesión
+                </Link>
               </div>
-              {error && <p className="text-red-500 text-center mt-4">{error}</p>}
-            </form>
-            <div className="text-gray-400 text-center mt-6">
-              Ya tienes una cuenta?{' '}
-              <Link href="/signin" className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out">
-                Inicia sesión
-              </Link>
             </div>
           </div>
         </div>
