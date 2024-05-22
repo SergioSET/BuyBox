6c2821ea154e3cf9a3b4e9ff1f60afdc8b0b4cd8
Steban01 <esteban.cortes@correounivalle.edu.co>
2024-05-22 18:04:46 -0500

    menu dashboard

--- Cliente/components/navbar.tsx
+++ Cliente/components/navbar.tsx
@@ -1,8 +1,7 @@
-"use client"; 
-
+'use client'
 import React, { useState } from 'react';
 
-const Navbar = () => {
+const Navbar = ({  }) => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -18,18 +17,18 @@ const Navbar = () => {
         <button className="text-white mr-4">Botón 3</button>
         <button className="text-white">Botón 4</button>
       </div>
-      <div className="flex items-center relative">
-        <p className="text-white mr-4">Usuario</p>
-        <button onClick={toggleMenu} className="text-white">Menu</button>
-        {menuOpen && (
-          <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10000">
-            <ul>
-              <li className="block px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">Opción 1</li>
-              <li className="block px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">Opción 2</li>
-              <li className="block px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">Opción 3</li>
-            </ul>
-          </div>
-        )}
+      <div className="flex items-center">
+        <p className="text-white mr-4">{}</p>
+        <div className="relative">
+          <button className="text-white" onClick={toggleMenu}>Menu</button>
+          {menuOpen && (
+            <div className="absolute right-0 mt-2 bg-white border rounded shadow-lg">
+              <button className="block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left">Opción 1</button>
+              <button className="block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left">Opción 2</button>
+              <button className="block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left">Opción 3</button>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
