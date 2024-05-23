afa0b1b3f54268fc43e019c0c50befa3dfc5277a
Johandarkness <70907376+Johandarkness@users.noreply.github.com>
2024-05-22 19:31:42 -0500

    Creación dashboard-admin

---
+++ Cliente/app/dashboard-admin/page.tsx
@@ -0,0 +1,15 @@
+
+import Navbar_admin from '@/components/navbar-admin';
+import MainContent from '@/components/main.content';
+import Link from 'next/link'
+
+export default function Dashboard_Admin() {
+  return (
+    <>
+      <Navbar_admin />
+      <MainContent/>
+     
+    </>
+  )
+}
+
---
+++ Cliente/components/navbar-admin.tsx
@@ -0,0 +1,46 @@
+'use client'
+import React, { useState } from 'react';
+import Link from "next/link";
+
+const Navbar_admin = ({  }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleCerrarSesion = () => {
+    document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    window.location.href = '/';
+}
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className="bg-gray-800 p-4 flex justify-between items-center">
+      <div className="flex items-center">
+        <h1 className="text-white mr-4">Dashboard</h1>
+        <button className="text-white mr-4">Pedidos</button>
+        <button className="text-white mr-4">Usuarios</button>
+      </div>
+      <div className="flex items-center">
+        <p className="text-white mr-4">{}</p>
+        <div className="relative">
+          <button className="text-white" onClick={toggleMenu}>Menu</button>
+          {menuOpen && (
+            <div className="absolute right-0 mt-2 bg-white border rounded shadow-lg">
+               <Link href="/perfil" className="block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left">
+                            Perfil
+                        </Link>
+
+                        <button className="block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left" onClick={handleCerrarSesion}>
+                            Cerrar Sesión
+                        </button>
+              <button className="block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left">Cerrar sesión</button>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar_admin;
