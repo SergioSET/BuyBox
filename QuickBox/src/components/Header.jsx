fafb8294738e4a50a3fbc3f46fabd9bc848ae5f4
Steban01 <esteban.cortes@correounivalle.edu.co>
2024-06-09 12:13:15 -0500

    navbar renovado

--- QuickBox/src/components/Header.jsx
+++ QuickBox/src/components/Header.jsx
@@ -1,3 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FaHeadphones } from "react-icons/fa6";
+import { FaRegEnvelope } from "react-icons/fa6";
+import { HiMiniBars3BottomLeft } from "react-icons/hi2";
+import { FaHeart } from "react-icons/fa6";
+import { AiFillShopping } from "react-icons/ai";
+import { FaSun } from "react-icons/fa6";
+import { FaMoon } from "react-icons/fa6";
+import { FaWindowClose } from "react-icons/fa";
+
+import "../styles/Header.css";
+import { useDispatch, useSelector } from "react-redux";
+import { changeMode } from "../features/auth/authSlice";
 import { store } from "../store";
 import axios from "axios";
 import { clearWishlist, updateWishlist } from "../features/wishlist/wishlistSlice";
@@ -7,13 +21,49 @@ import shopping_cart from '../assets/cart-shopping-solid.svg';  // Ajusta la rut
 
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-@@ -61,9 +64,10 @@ const Header = () => {
+  const loginState = localStorage.getItem("isLoggedIn") ? true : false;
+
+  const { amount } = useSelector((state) => state.cart);
+  const { total } = useSelector((state) => state.cart);
+  const [id, setId] = useState(localStorage.getItem("id"));
+  const dispatch = useDispatch();
+  const { darkMode } = useSelector((state) => state.auth);
+
+
+  // const fetchWishlist = async () => {
+  //   if (loginState) {
+  //     try {
+  //       const getResponse = await axios.get(`http://localhost:8080/user/${localStorage.getItem("id")}`);
+  //       const userObj = getResponse.data;
+
+  //       store.dispatch(updateWishlist({ userObj }));
+
+
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   } else {
+  //     store.dispatch(clearWishlist());
+  //   }
+
+  // };
+
+
+  useEffect(() => {
+    setIsLoggedIn(loginState);
+
+    // fetchWishlist();
+
+  }, [loginState]);
+
+  return (
+    <>
+      <div className="navbar bg-base-100 max-w-7xl mx-auto">
+        <div className="flex-1">
+          <Link
             to="/"
             className="btn btn-ghost normal-case text-2xl font-black text-accent-content"
           >
-            <AiFillShopping />
-            BuyBox
-          </Link>
           <img src={logo} alt="Logo" className="h-8 w-8 mr-2" />
 
           BuyBox
@@ -21,30 +71,74 @@ const Header = () => {
         </div>
         <div className="flex-none">
           {/* <Link
-@@ -100,20 +104,8 @@ const Header = () => {
+            to="/search"
+            className="btn btn-ghost btn-circle text-accent-content"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </Link> */}
+          {/* <button
+            className="text-accent-content btn btn-ghost btn-circle text-xl"
+            onClick={() => dispatch(changeMode())}
+          >
+            {darkMode ? <FaSun /> : <FaMoon />}
+          </button> */}
+          {/* <Link
+            to="/wishlist"
+            className="btn btn-ghost btn-circle text-accent-content"
+          >
+            <FaHeart className="text-xl" />
+          </Link> */}
           <div className="dropdown dropdown-end">
             <Link to="/cart" className="btn btn-ghost btn-circle">
               <div className="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  />
-                </svg>
-
+                
               <img src={shopping_cart} alt="Logo" className="h- w-6 mr-0.7" />
               </div>
             </Link>
           </div>
-@@ -150,10 +142,11 @@ const Header = () => {
+          {isLoggedIn && (
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img src="https://xsgames.co/randomusers/assets/avatars/male/60.jpg" />
+                </div>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <Link
+                    to="/user-profile"
+                    className="justify-between text-accent-content"
+                  >
+                    Perfil
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/order-history" className="text-accent-content">
+                    Casillero
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/login" className="text-accent-content">
+                    Cerrar Sesión
+                  </Link>
+                </li>
+              </ul>
             </div>
           )} {!isLoggedIn && (
             <div className="container text-2xl navlinks-container">
@@ -52,138 +146,110 @@ const Header = () => {
               <NavLink className="Login1" to="/login">
                 Iniciar sesión
               </NavLink>
-              <span className="Sbarra">/</span>
               <span className="Sbarra"></span>
               <NavLink className="Login" to="/register">
                 Registrarse
               </NavLink>
-@@ -229,7 +222,7 @@ const Header = () => {
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="navbar-bottom-menu border-y border-gray-800">
+        <div className="drawer">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+
+            {/* Page content here */}
+            <label htmlFor="my-drawer" className="btn drawer-button">
+              <HiMiniBars3BottomLeft className="text-4xl" />
+            </label>
+          </div>
+          <div className="drawer-side z-10">
+            <label
+              htmlFor="my-drawer"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+
+            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content mt-4">
+              <label htmlFor="my-drawer" className="btn drawer-button">
+                <FaWindowClose className="text-3xl ml-auto" />
+              </label>
+              {/* Sidebar content here */}
+              <li className="text-xl">
+                <NavLink className="text-accent-content" to="/">
+                  Página principal
+                </NavLink>
+              </li>
+              <li className="text-xl">
+                <NavLink className="text-accent-content" to="/shop">
+                  Tienda
+                </NavLink>
+              </li>
+              <li className="text-xl">
+                <NavLink className="text-accent-content" to="/locker">
+                  Casillero
+                </NavLink>
+              </li>
+              <li className="text-xl">
+                <NavLink className="text-accent-content" to="/about-us">
+                  Acerca de nosotros
+                </NavLink>
+              </li>
+              {/* <li className="text-xl">
+                <NavLink className="text-accent-content" to="/contact">
+                  Contact
+                </NavLink>
+              </li> */}
+              {/* {!isLoggedIn && (
+                <>
+                  <li className="text-xl">
+                    <NavLink className="text-accent-content" to="/login">
+                      Login
+                    </NavLink>
+                  </li>
+                  <li className="text-xl">
+                    <NavLink className="text-accent-content" to="/register">
+                      Register
+                    </NavLink>
+                  </li>
+                </>
+              )} */}
+            </ul>
+          </div>
+        </div>
 
         <div className="container text-2xl navlinks-container">
           <NavLink className="NavOp" to="/">
-            Página principal
             Inicio
           </NavLink>
           <NavLink className="NavOp" to="/shop">
             Tienda
-  91 changes: 75 additions & 16 deletions91  
-QuickBox/src/styles/Header.css
-Original file line number	Diff line number	Diff line change
-@@ -75,6 +75,10 @@
-    font-size: 18px;
-    color: lightgrey;
-    transition: color 0.3s ease; /* Transición suave de color durante 0.3 segundos */
-    font-family: 'Nunito Sans', sans-serif;
-    border: 2px solid #2563eb;
-    padding: 10px 20px; 
-    border-radius: 20px;
-}
+          </NavLink>
+          <NavLink className="NavOp" to="/locker">
+            Casillero
+          </NavLink>
+          <NavLink className="NavOp" to="/about-us">
+            Acerca de nosotros
+          </NavLink>
+          {/* <NavLink className="text-accent-content" to="/contact">
+            Contact
+          </NavLink> */}
+          {/* {!isLoggedIn && (
+            <>
+              <NavLink className="text-accent-content" to="/login">
+                Login
+              </NavLink>
+              <NavLink className="text-accent-content" to="/register">
+                Register
+              </NavLink>
+            </>
+          )} */}
+        </div>
+      </div>
+    </>
+  );
+};
 
-.Login:hover {
-@@ -83,36 +87,91 @@
-
-.Login1 {
-    font-size: 18px;
-    color: lightgrey;
-    color: white;
-    background-color: #2596be; /* Color de fondo del botón */
-    transition: color 0.3s ease; /* Transición suave de color durante 0.3 segundos */
-    margin-left: 10px;
-    border: 2px; /* Borde del botón */
-    font-family: 'Nunito Sans', sans-serif;
-    border-radius: 20px; /* Borde redondeado */
-    padding: 10px 20px; /* Padding para un mejor aspecto del botón */
-}
-
-.Login1:hover {
-    color: #2563eb; /* Cambia el color al pasar el mouse por encima */
-}
-
-
-.NavOp {
-    font-size: 20px;
-    display: block;
-    position: relative;
-    text-decoration: none; /* Desactiva la subrayado predeterminado */
-    color: white; /* Cambia el color del texto según lo necesites */
-    overflow: hidden;
-    text-decoration: none;
-    color: white;
-    font-family: 'Nunito Sans', sans-serif;
-    font-size: 20px;
-}
-
-.NavOp span {
-    display: block;
-    width: 100%;
-    transition: transform 0.5s;
-}
-
-.NavOp span:nth-child(1) {
-    color: #1abc9c;
-    background-color: rgba(255, 255, 255, 0.1);
-    font-size: 32px;
-}
-
-.NavOp span:nth-child(2) {
-    background-color: #1abc9c;
-    height: 100%;
-    position: absolute;
-    top: 100%;
-    left: 0;
-    width: 100%;
-}
-
-.NavOp:hover span:nth-child(1) {
-    transform: translateY(-100%);
-}
-
-.NavOp:hover span:nth-child(2) {
-    transform: translateY(-100%);
-}
-
-.NavOp::before,
-.NavOp::after {
-    content: '';
-    position: absolute;
-    bottom: 0;
-    height: 4px;
-    background: #2563eb;
-    transition: all 0.5s ease;
-}
-
-.NavOp::before {
-    width: 0;
-    height: 2px;
-    display: block;
-    margin-top: 5px;
-    right: 0;
-    background: #2563eb; /* Cambia el color según lo necesites */
-    transition: width 0.3s ease, left 0.3s ease;
-    left: 50%;
-    transform: translateX(-50%);
-}
-
-.NavOp::after {
-    width: 0;
-    right: 50%;
-    transform: translateX(50%);
-}
-
-.NavOp:hover::before,
-.NavOp:hover::after {
-    width: 100%;
-    right: 0;
-}
-    width: 50%;
-}
-
-.gif-image {
-    display: none;
-
-  }
-
-  .btn:hover .gif-image {
-    display: inline-block;
-
-  }
+export default Header;
