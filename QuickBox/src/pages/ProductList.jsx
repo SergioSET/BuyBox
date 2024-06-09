dcee123cd3d3385a483cfb5f37a289f40e27edd1
Alejandro Rodriguez <alejandro.marulanda@correounivalle.edu.co>
2024-06-09 18:48:27 -0500

    olverlaid image products

--- QuickBox/src/pages/ProductList.jsx
+++ QuickBox/src/pages/ProductList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useLoaderData, useNavigate } from 'react-router-dom';
 import '../../src/styles/dashboard-user.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faTrashAlt, faEye, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faTrashAlt, faEye, faPlus, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 export const productlistloader = async ({ request }) => {
     try {
@@ -29,6 +29,7 @@ export default function ProductList() {
     const products = useLoaderData();
     const navigate = useNavigate();
     const [visibleImages, setVisibleImages] = useState({});
+    const [modalImage, setModalImage] = useState(null);
 
     const handleCreate = () => {
         navigate(`/admin/new-product`);
@@ -61,76 +62,130 @@ export default function ProductList() {
         }
     };
 
-    const toggleImageVisibility = (id) => {
-        setVisibleImages((prevState) => ({
+    const toggleImageVisibility = (id, imgSrc = null) => {
+        setModalImage(imgSrc);
+        setVisibleImages(prevState => ({
             ...prevState,
             [id]: !prevState[id],
         }));
     };
 
+    const closeModal = () => {
+        setModalImage(null);
+    };
+
+
+    const modalStyles = {
+        display: 'flex', // Usa flexbox para el modal
+        justifyContent: 'center', // Centra horizontalmente
+        alignItems: 'center', // Centra verticalmente
+        position: 'fixed',
+        zIndex: 1,
+        left: 0,
+        top: 0,
+        width: '100%',
+        height: '100%',
+        overflow: 'auto',
+        backgroundColor: 'rgba(0,0,0,0.7)',
+    };
+
+    const modalContentStyles = {
+        backgroundColor: '#fefefe',
+        padding: '20px',
+        border: '1px solid #888',
+        width: 'auto', // Ancho automático basado en el contenido
+        maxWidth: '600px', // Máximo ancho permitido
+        textAlign: 'center',
+    };
+
+    const closeStyles = {
+        color: '#aaa',
+        float: 'right',
+        fontSize: '28px',
+        fontWeight: 'bold',
+        cursor: 'pointer',
+    };
+
+    const imageStyles = {
+        width: 'auto',
+        maxWidth: '100%',
+        maxHeight: '80vh',
+    };
+
+    const linkStyles = {
+        display: 'block',
+        marginTop: '10px',
+        color: '#007bff',
+        textDecoration: 'none',
+        cursor: 'pointer',
+    };
+
     return (
         <>
             <section className="relative">
-    <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-            <div className="tabla-con-divisiones-container"> {/* Línea agregada */}
-                <button onClick={handleCreate} className="btn btn-primary mb-4">
-                    <FontAwesomeIcon icon={faPlus} />Crear nuevo producto
-                </button>
-                {products.length === 0 ? (
-                    <p>No hay productos disponibles.</p>
-                ) : (
-                    <table className="tabla-con-divisiones">
-                        <thead>
-                            <tr>
-                                <th className="table-header" style={{ width: '15%', textAlign: 'center'}}>Nombre</th>
-                                <th className="table-header" style={{ width: '27%', textAlign: 'center'}}>Descripción</th>
-                                <th className="table-header" style={{ width: '16%', textAlign: 'center'}}>Imagen</th>
-                                <th className="table-header" style={{ width: '7%', textAlign: 'center'}}>Precio</th>
-                                <th className="table-header" style={{ width: '15%', textAlign: 'center'}}>Acciones</th>
-                            </tr>
-                        </thead>
-                        <tbody className="table-body">
-                            {products.map((product) => (
-                                <tr key={product.id}>
-                                    <td>{product.name}</td>
-                                    <td>{product.description}</td>
-                                    <td style={{ textAlign: 'center', verticalAlign: 'middle' }}>
-                                        {visibleImages[product.id] ? (
-                                            <img
-                                                src={product.img}
-                                                alt={product.name}
-                                                onClick={() => toggleImageVisibility(product.id)}
-                                                style={{ cursor: 'pointer' }}
-                                            />
-                                        ) : (
-                                            <button
-                                                onClick={() => toggleImageVisibility(product.id)}
-                                                className="btn btn-primary"
-                                            >
-                                                <FontAwesomeIcon icon={faEye} />Mostrar imagen
-                                            </button>
-                                        )}
-                                    </td>
-                                    <td style={{ textAlign: 'center', verticalAlign: 'middle' }}>{product.price}</td>
-                                    <td style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                        <button onClick={() => handleEdit(product.id)} className="btn btn-primary" style={{ marginRight: '5px' }}>
-                                            <FontAwesomeIcon icon={faEdit} /> Editar
-                                        </button>
-                                        <button onClick={() => handleDelete(product.id, product.name)} className="btn btn-red" style={{ marginRight: '5px' }}>
-                                            <FontAwesomeIcon icon={faTrashAlt} /> Borrar
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                )}
-            </div>
-        </div>
-    </div>
-</section>
+                <div className="max-w-6xl mx-auto px-4 sm:px-6">
+                    <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+                        <div className="tabla-con-divisiones-container">
+                            <button onClick={handleCreate} className="btn btn-primary mb-4">
+                                <FontAwesomeIcon icon={faPlus} /> Crear nuevo producto
+                            </button>
+                            {products.length === 0 ? (
+                                <p>No hay productos disponibles.</p>
+                            ) : (
+                                <table className="tabla-con-divisiones">
+                                    <thead>
+                                        <tr>
+                                            <th className="table-header" style={{ width: '15%', textAlign: 'center' }}>Nombre</th>
+                                            <th className="table-header" style={{ width: '27%', textAlign: 'center' }}>Descripción</th>
+                                            <th className="table-header" style={{ width: '16%', textAlign: 'center' }}>Imagen</th>
+                                            <th className="table-header" style={{ width: '7%', textAlign: 'center' }}>Precio</th>
+                                            <th className="table-header" style={{ width: '15%', textAlign: 'center' }}>Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="table-body">
+                                        {products.map((product) => (
+                                            <tr key={product.id}>
+                                                <td>{product.name}</td>
+                                                <td>{product.description}</td>
+                                                <td style={{ textAlign: 'center', verticalAlign: 'middle' }}>
+                                                    <button
+                                                        onClick={() => toggleImageVisibility(product.id, product.img)}
+                                                        className="btn btn-primary"
+                                                    >
+                                                        <FontAwesomeIcon icon={faEye} /> Mostrar imagen
+                                                    </button>
+                                                </td>
+                                                <td style={{ textAlign: 'center', verticalAlign: 'middle' }}>{product.price}</td>
+                                                <td style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                                    <button onClick={() => handleEdit(product.id)} className="btn btn-primary" style={{ marginRight: '5px' }}>
+                                                        <FontAwesomeIcon icon={faEdit} /> Editar
+                                                    </button>
+                                                    <button onClick={() => handleDelete(product.id, product.name)} className="btn btn-red" style={{ marginRight: '5px' }}>
+                                                        <FontAwesomeIcon icon={faTrashAlt} /> Borrar
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+          
+            {modalImage && (
+                <div id="myModal" style={modalStyles} onClick={closeModal}>
+                    <div style={modalContentStyles} onClick={e => e.stopPropagation()}>
+                        <span style={closeStyles} onClick={closeModal}>&times;</span>
+                        <img src={modalImage} alt="Product" style={imageStyles} />
+                        <a href={modalImage} target="_blank" rel="noopener noreferrer" style={linkStyles}>
+                            <FontAwesomeIcon icon={faExternalLinkAlt} /> Ver en pestaña nueva
+                        </a>
+                    </div>
+                </div>
+            )}
         </>
-        
     );
 }
