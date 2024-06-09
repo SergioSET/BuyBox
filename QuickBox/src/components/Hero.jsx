c0a0fad0e13c1dd4b914408d6340d433a6392ef7
Steban01 <esteban.cortes@correounivalle.edu.co>
2024-06-09 17:58:06 -0500

    finalizacion de homepage

--- QuickBox/src/components/Hero.jsx
+++ QuickBox/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="max-w-xl">
         <h1 className="text-6xl font-bold max-md:text-4xl text-accent-content">Compras y Envíos sin Complicaciones</h1>
         <p className="py-6 text-2xl max-md:text-lg text-accent-content">Gestiona tus compras desde EE.UU. de forma fácil y clara. Administra tu casillero, estima costos y sigue tus pedidos en tiempo real.</p>
-        <Link to="/shop" className="btn btn-wide bg-blue-600 hover:bg-blue-500 text-white">¡Compra ahora!</Link>
+        <Link to="/shop" className="btn btn-wide bg-blue-600 hover:bg-blue-500 text-white button-3d">¡Compra ahora!</Link>
       </div>
     </div>
   </div>
