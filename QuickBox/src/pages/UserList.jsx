db2b68b87bdd7a21dcf9a56ce25c2911e96ffe3d
Alejandro Rodriguez <alejandro.marulanda@correounivalle.edu.co>
2024-06-09 00:38:45 -0500

    Front Tables

--- QuickBox/src/pages/UserList.jsx
+++ QuickBox/src/pages/UserList.jsx
@@ -65,47 +65,49 @@ export default function UserList() {
 
     return (
         <>
-            <section className="relative">
+           <section className="relative">
     <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-            <button onClick={handleCreate} className="btn btn-primary mb-4">
-                <FontAwesomeIcon icon={faPlus} />Crear nuevo usuario
-            </button>
-            {users.length === 0 ? (
-                <p>No hay usuarios disponibles.</p>
-            ) : (
-                <table className="tabla-con-divisiones">
-                    <thead>
-                        <tr>
-                            <th className="table-header" style={{ width: '15%', textAlign: 'center'}}>Nombre</th>
-                            <th className="table-header" style={{ width: '10%', textAlign: 'center'}}>Rol</th>
-                            <th className="table-header" style={{ width: '20%', textAlign: 'center'}}>Correo electrónico</th>
-                            <th className="table-header" style={{ width: '18%', textAlign: 'center'}}>Dirección</th>
-                            <th className="table-header" style={{ width: '15%', textAlign: 'center' }}>Teléfono</th>
-                            <th className="table-header" style={{ width: '25%', textAlign: 'center'}}>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody className="table-body">
-                        {users.map((user) => (
-                            <tr key={user.id}>
-                                <td>{user.name}</td>
-                                <td style={{ textAlign: 'center', verticalAlign: 'middle' }}>{user.role}</td>
-                                <td>{user.email}</td>
-                                <td style={{ textAlign: 'center', verticalAlign: 'middle' }}>{user.address}</td>
-                                <td style={{ textAlign: 'center', verticalAlign: 'middle' }}>{user.phone}</td>
-                                <td style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                    <button onClick={() => handleEdit(user.id)} className="btn btn-primary" style={{ marginRight: '5px' }}>
-                                        <FontAwesomeIcon icon={faEdit} /> Editar
-                                    </button>
-                                    <button onClick={() => handleDelete(user.id)} className="btn btn-error" style={{ marginLeft: '5px' }}>
-                                        <FontAwesomeIcon icon={faTrashAlt} /> Borrar
-                                    </button>
-                                </td>
+            <div className="tabla-con-divisiones-container"> {/* Línea agregada */}
+                <button onClick={handleCreate} className="btn btn-primary mb-4">
+                    <FontAwesomeIcon icon={faPlus} />Crear nuevo usuario
+                </button>
+                {users.length === 0 ? (
+                    <p>No hay usuarios disponibles.</p>
+                ) : (
+                    <table className="tabla-con-divisiones">
+                        <thead>
+                            <tr>
+                                <th className="table-header" style={{ width: '15%', textAlign: 'center' }}>Nombre</th>
+                                <th className="table-header" style={{ width: '10%', textAlign: 'center' }}>Rol</th>
+                                <th className="table-header" style={{ width: '20%', textAlign: 'center' }}>Correo electrónico</th>
+                                <th className="table-header" style={{ width: '18%', textAlign: 'center' }}>Dirección</th>
+                                <th className="table-header" style={{ width: '15%', textAlign: 'center' }}>Teléfono</th>
+                                <th className="table-header" style={{ width: '25%', textAlign: 'center' }}>Acciones</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            )}
+                        </thead>
+                        <tbody className="table-body">
+                            {users.map((user) => (
+                                <tr key={user.id}>
+                                    <td>{user.name}</td>
+                                    <td style={{ textAlign: 'center', verticalAlign: 'middle' }}>{user.role}</td>
+                                    <td>{user.email}</td>
+                                    <td style={{ textAlign: 'center', verticalAlign: 'middle' }}>{user.address}</td>
+                                    <td style={{ textAlign: 'center', verticalAlign: 'middle' }}>{user.phone}</td>
+                                    <td className="actions-cell">
+                                        <button onClick={() => handleEdit(user.id)} className="btn btn-primary" style={{ marginRight: '5px' }}>
+                                            <FontAwesomeIcon icon={faEdit} /> Editar
+                                        </button>
+                                        <button onClick={() => handleDelete(user.id)} className="btn btn-red" style={{ marginLeft: '5px' }}>
+                                            <FontAwesomeIcon icon={faTrashAlt} /> Borrar
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
+            </div>
         </div>
     </div>
 </section>
