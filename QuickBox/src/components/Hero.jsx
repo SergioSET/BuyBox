59c8612944910102459715be4d28104f24ab82d2
Steban01 <esteban.cortes@correounivalle.edu.co>
2024-06-09 16:37:59 -0500

    hero y stats renovados

--- QuickBox/src/components/Hero.jsx
+++ QuickBox/src/components/Hero.jsx
@@ -5,13 +5,9 @@ const Hero = () => {
     <div className="hero bg-base-200 bg-blend-overlay">
     <div className="hero-content text-center">
       <div className="max-w-xl">
-        <h1 className="text-6xl font-bold max-md:text-4xl text-accent-content">Best Clothing Shop Of The Year!</h1>
-        <p className="py-6 text-2xl max-md:text-lg text-accent-content">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-          et a id nisi.
-        </p>
-        <Link to="/shop" className="btn btn-wide bg-blue-600 hover:bg-blue-500 text-white">Shop Now</Link>
+        <h1 className="text-6xl font-bold max-md:text-4xl text-accent-content">Compras y Envíos sin Complicaciones</h1>
+        <p className="py-6 text-2xl max-md:text-lg text-accent-content">Gestiona tus compras desde EE.UU. de forma fácil y clara. Administra tu casillero, estima costos y sigue tus pedidos en tiempo real.</p>
+        <Link to="/shop" className="btn btn-wide bg-blue-600 hover:bg-blue-500 text-white">¡Compra ahora!</Link>
       </div>
     </div>
   </div>
