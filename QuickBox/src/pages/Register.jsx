3662907d19b2a1b224a582dbe0ed13a37f7c90c5
Alejandro Rodriguez <alejandro.marulanda@correounivalle.edu.co>
2024-06-09 23:31:33 -0500

    Cambios Registro y Login

--- QuickBox/src/pages/Register.jsx
+++ QuickBox/src/pages/Register.jsx
@@ -72,13 +72,13 @@ const Register = () => {
   };
   return (
     <>
-      <SectionTitle title="Register" path="Home | Register" />
+      <SectionTitle title="Registro" path="Home | Registro" />
       <div className="flex flex-col justify-center sm:py-12">
         <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
           <div className="bg-dark border border-gray-600 shadow w-full rounded-lg divide-y divide-gray-200">
             <form className="px-5 py-7" onSubmit={handleSubmit}>
               <label className="font-semibold text-sm pb-1 block text-accent-content">
-                Name
+                Nombre
               </label>
               <input
                 type="text"
@@ -98,7 +98,7 @@ const Register = () => {
                 required={true}
               />
               <label className="font-semibold text-sm pb-1 block text-accent-content">
-                Phone
+                Número de Teléfono
               </label>
               <input
                 type="tel"
@@ -108,7 +108,7 @@ const Register = () => {
                 required={true}
               />
               <label className="font-semibold text-sm pb-1 block text-accent-content">
-                Adress
+                Dirección
               </label>
               <input
                 type="text"
@@ -118,7 +118,7 @@ const Register = () => {
                 required={true}
               />
               <label className="font-semibold text-sm pb-1 block text-accent-content">
-                Password
+                Contraseña
               </label>
               <input
                 type="password"
@@ -128,7 +128,7 @@ const Register = () => {
                 required={true}
               />
               <label className="font-semibold text-sm pb-1 block text-accent-content">
-                Repeat Password
+                Repetir Contraseña
               </label>
               <input
                 type="password"
@@ -141,7 +141,7 @@ const Register = () => {
                 type="submit"
                 className="transition duration-200 bg-blue-600 hover:bg-blue-500 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
               >
-                <span className="inline-block mr-2">Register</span>
+                <span className="inline-block mr-2">Registrarse</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -165,7 +165,7 @@ const Register = () => {
               className="btn btn-neutral text-white"
               onClick={() => window.scrollTo(0, 0)}
             >
-              Already have an account? Please login.
+              ¿Ya tienes una cuenta? Inicia sesión.
             </Link>
           </div>
         </div>
