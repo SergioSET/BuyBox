2db31a24abab617a6bd38d4da131f83eb5ec4295
Johandarkness <70907376+Johandarkness@users.noreply.github.com>
2024-06-11 20:21:02 -0500

    Update thankyou

--- Servidor/src/db.js
+++ Servidor/src/db.js
@@ -2,7 +2,7 @@ import {createPool} from 'mysql2/promise'
 export const pool = createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: '1234',
     port: '3306',
     database: 'buyboxdb'
 })
