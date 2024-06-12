9e223365830f91792d165f1503ad8816a553827e
Juan Pablo Pantoja <juan.pablo.pantoja@correounivalle.edu.co>
2024-06-12 12:19:57 -0500

    Update shop

--- QuickBox/src/components/ProductElement.jsx
+++ QuickBox/src/components/ProductElement.jsx
@@ -4,32 +4,40 @@ import { Link } from "react-router-dom";
 import { addToCart } from "../features/cart/cartSlice";
 
 const ProductElement = ({ id, title, image, rating, price, brandName }) => {
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+  const dispatch = useDispatch();
+
   const product = {
     id, title, image, rating, price, brandName, amount: 1
   };
+
   return (
     <div className="max-w-2xl bg-100">
-      <div className="rounded-lg max-w-sm bg-100">
-        <Link to={`/shop/product/${id}`} onClick={() => window.scrollTo(0, 0)}>
-          <div className="image-container">
-            <img
-              className="product-image"
-              src={`${image}`}
-              alt="product image"
-            />
-          </div>
-        </Link>
-        <div className="px-center pb-15">
+      {isLoggedIn ? (
+        <div className="rounded-lg max-w-sm bg-100">
           <Link to={`/shop/product/${id}`} onClick={() => window.scrollTo(0, 0)}>
-            <h3 className="font-semibold text-xl tracking-tight mb-5 text-accent-content">
-              {title}
-            </h3>
+            <div className="image-container">
+              <img
+                className="product-image"
+                src={`${image}`}
+                alt="product image"
+              />
+            </div>
           </Link>
-          <div className="flex items-center justify-between">
-            <span className=" text-3xl font-bold text-accent-content">{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0 }).format(price)}</span>
+          <div className="px-center pb-15">
+            <Link to={`/shop/product/${id}`} onClick={() => window.scrollTo(0, 0)}>
+              <h3 className="font-semibold text-xl tracking-tight mb-5 text-accent-content">
+                {title}
+              </h3>
+            </Link>
+            <div className="flex items-center justify-between">
+              <span className=" text-3xl font-bold text-accent-content">{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP', minimumFractionDigits: 0 }).format(price)}</span>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <Link to="/login">Inicia sesión para ver este producto</Link>
+      )}
     </div>
   );
 };
